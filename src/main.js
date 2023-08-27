eb3587d19b52a47c7a6544e79556cfdab496757b
Wahyu Kusumo <54056573+wahyukusumo@users.noreply.github.com>
2023-08-27 17:27:15 +0700

    Fix script src

--- src/main.js
+++ src/main.js
@@ -1,14 +1,12 @@
 import { createApp } from 'vue'
-import './index.css'
+import '@/index.css'
 import { autoAnimatePlugin } from '@formkit/auto-animate/vue'
-import drag from "v-drag"
-import App from './App.vue'
+import App from '@/App.vue'
 
 const app = createApp(App)
 
 app.config.compilerOptions.isCustomElement = (tag) => tag.startsWith('ion-')
 app.config.warnHandler = () => null;
-app.use(drag)
 app.use(autoAnimatePlugin)
 
 app.mount('#app')
