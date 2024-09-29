bf1063adfeeed9e3b84d7c663a6b15d2fe151a03
Wahyu Kusumo <54056573+wahyukusumo@users.noreply.github.com>
2024-09-30 03:55:09 +0700

    Added dist

--- src/components/BaseApp/TitleBarButton.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <ion-icon name="" size="small"
-    class="bg-red-600 hover:bg-red-400 rounded-full cursor-pointer text-red-600 hover:text-black hover:scale-110 transition-all">
-  </ion-icon>
-</template>
--- src/components/Card.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <!-- <div class="p-2 bg-white dark:bg-[#231d26] dark:text-gray-200 border-b border-r border-gray-300 dark:border-black w-full last:border-none"> -->
-  <div
-    class="p-2 bg-white dark:bg-[#231d26] dark:text-gray-200 border-b border-r border-gray-300 dark:border-black w-full">
-    <slot></slot>
-  </div>
-</template>
