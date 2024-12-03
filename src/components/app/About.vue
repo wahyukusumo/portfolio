f562e845ab1ecbfaba2471a60f714dac2ebeffe8
Wahyu Kusumo <dev@wahyukusumo.com>
2024-12-04 01:37:06 +0700

    Add close button when full view of image

--- src/components/app/About.vue
+++ src/components/app/About.vue
@@ -5,30 +5,44 @@ import MyButton from '@/components/DefaultButton.vue'
 </script>
 
 <template>
-  <Icon :appName="appdata.apps[0].name.en" :bgImage="'bg-[url(@/assets/icons/about.webp)]'"
-    @windowFull="(isParentFull) => (isFull = isParentFull)">
+  <Icon
+    :appName="appdata.apps[0].name.en"
+    :bgImage="'bg-[url(@/assets/icons/about.webp)]'"
+    @windowFull="(isParentFull) => (isFull = isParentFull)"
+  >
     <div class="flex flex-col" :class="{ 'md:grid-cols-1': isFull }">
       <Card class="flex flex-col justify-center items-center py-5 h-full lg:border-b-0">
         <!-- Cover image -->
-        <div class="bg-[url('@/assets/profile/profile_banner_small.webp')] bg-center bg-cover w-full z-10"
-          :class="{ 'xl:h-[20vh] 2xl:h-[30vh]': isFull, 'xl:h-[20vh]': !isFull }"></div>
+        <div
+          class="bg-[url('@/assets/profile/profile_banner_small.webp')] bg-center bg-cover w-full z-10"
+          :class="{ 'xl:h-[20vh] 2xl:h-[30vh]': isFull, 'xl:h-[20vh]': !isFull }"
+        ></div>
 
         <!-- Info Container -->
-        <div class="flex justify-center flex-col items-center" :class="{
-          'xl:absolute xl:mt-[45vh]': isFull,
-          'xl:absolute xl:mt-[43vh]': !isFull
-        }">
+        <div
+          class="flex justify-center flex-col items-center"
+          :class="{
+            'xl:absolute xl:mt-[45vh]': isFull,
+            'xl:absolute xl:mt-[43vh]': !isFull
+          }"
+        >
           <!-- Profile image -->
           <div
-            class="bg-[url('@/assets/profile/avatar.webp')] bg-center bg-white bg-dark-800 bg-cover h-64 w-64 md:w-48 md:h-48 z-20 rounded-full border-8 border-white dark:border-[#231d26]"
-            :class="{ 'border-white dark:border-[#231d26]': isFull }" />
+            class="bg-[url('@/assets/profile/avatar.webp')] bg-center bg-white bg-cover h-64 w-64 md:w-48 md:h-48 z-20 rounded-full border-8 border-white dark:border-[#231d26]"
+            :class="{ 'border-white dark:border-[#231d26]': isFull }"
+          />
 
           <!-- About -->
-          <p class="my-6 text-center z-20" :class="{ 'relative mx-[60vh]': isFull }"
-            v-html="appdata.profile.about.en" />
+          <p
+            class="my-6 text-center z-20"
+            :class="{ 'relative mx-[60vh]': isFull }"
+            v-html="appdata.profile.about.en"
+          />
 
           <!-- Contact -->
-          <div :class="{ 'flex gap-4': isFull, 'flex flex-wrap gap-4 justify-center mt-5': !isFull }">
+          <div
+            :class="{ 'flex gap-4': isFull, 'flex flex-wrap gap-4 justify-center mt-5': !isFull }"
+          >
             <MyButton v-for="social in appdata.socials" :key="social" class="gap-1">
               <ion-icon size="small" :name="social.icon" />
               {{ social.username }}
--- src/components/app/Photo.vue
+++ src/components/app/Photo.vue
@@ -1,38 +1,60 @@
 <script setup>
 import Icon from '@/components/Icon.vue'
 import Card from '@/components/Card.vue'
+import { XMarkIcon } from '@heroicons/vue/24/outline'
 </script>
 
 <template>
-  <Icon :appName="'Photos'" :bgImage="'bg-[url(@/assets/icons/camera_alt.webp)]'"
-    @windowFull="(isParentFull) => (isFull = isParentFull)">
-
+  <Icon
+    :appName="'Photos'"
+    :bgImage="'bg-[url(@/assets/icons/camera_alt.webp)]'"
+    @windowFull="(isParentFull) => (isFull = isParentFull)"
+  >
     <!-- Full Image View -->
     <div v-show="imgFullScreen" class="flex items-center justify-center w-full h-full">
-      <img :src="imgFullScreen" class="absolute z-20 shadow-md overflow-x-auto"
-        :class="{ 'h-[90vh]': isFull, 'h-[60vh]': !isFull }">
-      <div class="w-full h-[60vh] bg-gray-400 dark:bg-gray-700 blur-xl cursor-pointer" @click="imgFullScreen = false" />
+      <button
+        class="absolute z-30 p-3 m-4 top-0 right-0 rounded-full bg-white/40"
+        @click="imgFullScreen = false"
+      >
+        <XMarkIcon class="size-6 stroke-gray-600 hover:stroke-white" />
+      </button>
+      <img
+        :src="imgFullScreen"
+        class="absolute z-20 shadow-md overflow-x-auto"
+        :class="{ 'h-[90vh]': isFull, 'h-[60vh]': !isFull }"
+      />
+      <div
+        class="w-full h-[60vh] bg-gray-400 dark:bg-gray-700 blur-xl cursor-pointer"
+        @click="imgFullScreen = false"
+      />
     </div>
 
     <!-- Gallery View -->
-    <div v-show="!imgFullScreen" class="p-2 gap-2 grid grid-cols-1 md:grid-cols-3 xl:p-4 relative"
-      :class="{ 'xl:grid-cols-5': isFull }">
+    <div
+      v-show="!imgFullScreen"
+      class="p-2 gap-2 grid grid-cols-1 md:grid-cols-3 xl:p-4 relative"
+      :class="{ 'xl:grid-cols-5': isFull }"
+    >
       <div class="container mx-auto relative cursor-pointer" v-for="image in appdata.galleries">
-
-        <div class="h-80 xl:h-56 relative rounded-lg" :class="{ 'xl:h-[22rem]': isFull }"
-          @click="imgFullScreen = image.image">
+        <div
+          class="h-80 xl:h-56 relative rounded-lg"
+          :class="{ 'xl:h-[22rem]': isFull }"
+          @click="imgFullScreen = image.image"
+        >
           <!-- Image -->
-          <div class="absolute inset-0 bg-cover bg-center z-0 rounded-lg"
-            :style="{ backgroundImage: `url(${image.image})` }" />
+          <div
+            class="absolute inset-0 bg-cover bg-center z-0 rounded-lg"
+            :style="{ backgroundImage: `url(${image.image})` }"
+          />
           <!-- Description on Hover -->
-          <div class="opacity-0 hover:opacity-100 hover:rounded-lg duration-300 absolute inset-0
-            z-10 flex justify-center items-center text-center text-base text-white bg-black/30">
+          <div
+            class="opacity-0 hover:opacity-100 hover:rounded-lg duration-300 absolute inset-0 z-10 flex justify-center items-center text-center text-base text-white bg-black/30"
+          >
             <p>{{ image.caption }}</p>
           </div>
         </div>
       </div>
     </div>
-
   </Icon>
 </template>
 
